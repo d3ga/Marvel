9dd1d34d075afcd78335d27181ebb2f45b8b2469
dega_k <31686760+d3ga@users.noreply.github.com>
2018-02-12 13:03:29 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,64 +1,55 @@
-// focus the text field on load
-$('#input-name').focus()
-
-$('#search-form').submit((e) => {
-    e.preventDefault()
-})
-
 class marvelAPI {
   constructor() {
     this.apiKey = '8eb4391075c9034247c1cc9b86743ffc'
     this.apiUrl = 'https://gateway.marvel.com:443/v1/public'
-
+    
     this.params = {
       'orderBy': 'name',
-      'limit': 30,
+      'limit': 30,      
       'offset': 0,
       'apikey': this.apiKey
     }
   }
-
+  
   setLimit(amount) {
     this.params.limit = amount
   }
-
+  
   setOffset(offset) {
     this.params.offset = offset
   }
-
+  
   setParams(params) {
     this.params = $.extend(this.params, params)
   }
-
+  
   callApi(endpoint, params, callback) {
     const apiParams = $.extend(this.params, params)
-
+     
     $.getJSON(`${this.apiUrl}${endpoint}`, apiParams)
-      .done((data) => {
-        // console.log('getJSON', data)
+      .done((data) => { 
         callback(data)
-
+        console.log(data)
       })
       .fail((response) => {
         callback(null)
         console.error('Response', response)
       })
   }
-
+  
   searchByName(name, callback) {
     this.callApi(
       '/characters',
-
       {
         'name': name,
       },
       callback
     )
   }
-
+  
   nameStartsWith(name, callback) {
     delete this.params.name
-
+    
     this.callApi(
       '/characters',
       {
@@ -68,34 +59,31 @@ class marvelAPI {
     )
   }
 }
-
-class CharacterSearch {
+ 
+class CharacterSearch { 
   constructor() {
     this.marvelAPI = new marvelAPI()
     this.elements = {
       'form': $('#search-form'),
       'input': $('#search-input'),
-      'results': $('#results'),
-      'characterList': $('.character-list')
+      'results': $('#results')
     }
-
+    
     this.registerEvents()
   }
-
+  
   registerEvents() {
     this.elements.form.on('submit', (e) => {
       e.preventDefault()
-      const name = this.elements.input.val().trim()
-
+      const name = this.elements.input.val().trim();
+      
       this.marvelAPI.searchByName(
         name,
         (data) => {
           if (data.data.results.length !== 0) {
             this.showResults(data.data.results)
           } else {
-            this.marvelAPI.nameStartsWith(
-              name,
-              (data) => {
+            this.marvelAPI.nameStartsWith(name, (data) => {
               this.showResults(data.data.results)
             })
           }
@@ -103,55 +91,30 @@ class CharacterSearch {
       )
     })
   }
-
-  showResults(results) {
-    this.elements.characterList.html('')
-    if (results.length === 0) {
-
+  
+  showResults(data) {   
+    this.elements.results.html('')
+    
+    if (data.length === 0) {
       this.showError('Character not found in database!')
-
     } else {
       $('#error').remove()
-      results.forEach((item) => {
-
-        let comics = `<ul class="comics-list">`
-
-        item.comics.items.forEach((comic) => {
-          comics+= `<li class="list-item">${comic.name}</li>`
-        })
-
-        comics+=`</ul>`
-
-        this.elements.characterList.append(`
-          <li class="list-item" data-id="${item.id}">
-              <div class="name">${item.name}</div>
-              <div class="thumbnail">
-                  <img src="${item.thumbnail.path}.${item.thumbnail.extension}"
-                      width="250"
-                      height="250">
-              </div>
-              <div class="description text-center text-muted">
-                  ${item.description}
-              </div>
-
-              <a href="#title-comics" class="title-comics">Comics</a>
-
-              <div class="comics text-center text-muted hidden">
-                  ${comics}
-              </div>
-          </li>
+      data.forEach((item) => {
+        this.elements.results.append(`
+           <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">'${item.name}'</h4>
+                <p class="card-text">'${item.description}'</p>   
+             </div>
+          </div>
         `)
-     })
-
-     $('.list-item').on('click', (e) => {
-       $(e.currentTarget).find('.comics').toggleClass('hidden')
-     })
+     })  
     }
   }
 
   showError(message) {
     let alert = $('#error')
-
+    
     if (alert.length === 0) {
       this.elements.form.before('<div class="alert alert-danger" id="error"></div>')
       alert = $('#error')
